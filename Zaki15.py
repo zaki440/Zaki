d3a37c9064823d08e4e3f43e8295b68d0a4009df
zaki440 <157621655+zaki440@users.noreply.github.com>
2024-02-02 00:15:52 +0430

    Zaki15.py
    git clone https://github.com/zaki440/Zaki.git
    ls
    cd Zaki15
    ls
    python Zaki15.py

---
+++ Zaki15.py
@@ -0,0 +1,51 @@
+echo" "
+echo" "
+echo" "
+echo"This script is maked by : sofizaki takhari"
+echo" "
+echo" "
+echo" "
+echo"join my telegram channel"
+echo" "
+echo" "
+echo" "
+echo "instillation provess is strated"
+echo" "
+echo" "
+echo" "
+os.system('termux-setup-storage')
+os.system('pkg update')
+os.system('pkg upgrade')
+os.system('pkg install git')
+os.system('pkg install python')
+os.system('pkg install python2')
+os.system('pip2 install requests')
+os.system('pip2 install mechanize')
+os.system('pip2 install bs4')
+os.system('pip install requests')
+os.system('pip install bs4')
+os.system('pip install rich')
+os.system('pip install crypto')
+os.system('pip install mechanize')
+os.system('pip install cython')
+echo" "
+echo" "
+echo" "
+echo"all basic packages and tools installed"
+os.system('xdg-open https:t.me/SOFIZAKITAKHARI')
+echo" "
+echo" "
+echo"
+ _____     _    _ 
+|__  /__ _| | _(_)
+  / // _` | |/ / |
+ / /| (_| |   <| |
+/____\__,_|_|\_\_|
+echo"
+this tools is maked by sofizaki takhari 
+telegram: t.me/SOFIZAKITAKHARI
+youtube: https://www.youtube.com/@Sofizaki_and_Faried_technical
+exit
+
+
+
